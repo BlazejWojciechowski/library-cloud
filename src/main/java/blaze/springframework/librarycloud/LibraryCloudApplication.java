c6cfaeb56f03d89ae2b8efb75651b639d4dafe35
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-03-31 19:13:48 +0200

    initial commit

---
+++ src/main/java/blaze/springframework/librarycloud/LibraryCloudApplication.java
@@ -0,0 +1,13 @@
+package blaze.springframework.librarycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blaze/springframework/librarycloud/LibraryCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package blaze.springframework.librarycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
